44e0c0ecc76838a98c4d7d5785d501ca436f69b7
Kevin Aiken <Aiken.Kevin.M@gmail.com>
2019-06-06 16:55:21 -0400

    added gpio library

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,23 @@
 
 #[macro_use] extern crate rocket;
 
+extern crate sysfs_gpio;
+
+use std::thread::sleep;
+use std::time::Duration;
+use sysfs_gpio::Pin;
 
 #[get("/")]
 fn index() -> &'static str {
+    let in1 = Pin::new(16);
+    let in2 = Pin::new(26);
+    in1.with_exported(|| {
+            in1.set_value(0).unwrap();
+            sleep(Duration::from_millis(200));
+            in1.set_value(1).unwrap();
+            sleep(Duration::from_millis(200));
+    }).unwrap();
+
     "Hello, world!"
 }
 
@@ -12,3 +26,4 @@ fn index() -> &'static str {
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
 }
+
